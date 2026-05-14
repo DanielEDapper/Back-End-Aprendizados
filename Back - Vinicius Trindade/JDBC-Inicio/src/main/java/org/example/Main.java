25f01dcf8b4da7334c5cdf0b3e148e89ffa062c1
Daniel Erthal Dapper <danielerthaldapper2008@gmail.com>
2026-05-14 18:02:56 -0300

    Add files via upload

---
+++ Back - Vinicius Trindade/JDBC-Inicio/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main 
+{
+    static Scanner READ = new Scanner(System.in);
+    public static void main(String[] args) {
+       // int opcao = 0;
+       //System.out.println("""
+       //     Selecione uma opção: 
+       //     1 - Cadastrar contato
+       //     2 - Atualizar contato
+       //     3 - Buscar contatos
+       //     4 - Buscar contato por nome
+       //     5 - Deletar contato
+       //        """);
+       // opcao = READ.nextInt();
+       // READ.nextLine();
+    }
+}
